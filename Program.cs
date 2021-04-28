d15ad0211767518bdc16c69d0c32efef8dd3f646
ADVallespir <advallespir@gmail.com>
2021-04-28 10:02:59 -0300

    End

--- Program.cs
+++ Program.cs
@@ -7,6 +7,8 @@ namespace Guia_1_21
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine(DateTime.Now);
+            Console.ReadKey();
         }
     }
 }
